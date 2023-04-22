84a49b4ca83397054c2d9a773cfc05d6e81cfa55
zeoneo <zeo29021992@gmail.com>
2023-04-22 15:24:01 +0530

    Add cpp files to build

--- kernel/hello.cpp
+++ kernel/hello.cpp
@@ -1,11 +1,34 @@
 #include"hello.hpp"
 
+char * x = "welcome prakash";
+
 int get_1() {
     return 1;
 }
 
+class Hello
+{
+private:
+    /* data */
+public:
+    Hello(/* args */);
+    ~Hello();
+};
+
+Hello::Hello(/* args */)
+{
+}
+
+Hello::~Hello()
+{
+}
+
 extern "C" {
-    int my_function(int arg1, int arg2) {
-        return 0;
+    char * my_function(int arg1, int arg2) {
+        Hello y = Hello();
+        return x;
     }
 }
+
+
+
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,13 @@
 #include "../drivers/uart0.h"
 #include "hello.hpp"
 
-extern int my_function(int arg1, int arg2);
+extern char * my_function(int arg1, int arg2);
 
 int kernel_main(void) {
 
   uart_init();
 
   uart_puts("Hello World");
-
-  return my_function(get_1(), 0);
+  uart_puts(my_function(get_1(), 0));
+  return 0;
 }
