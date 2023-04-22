e1d2d5b9d6a72302c5bb4234ff391e6d717c7923
zeoneo <zeo29021992@gmail.com>
2023-04-22 17:08:59 +0530

    add zeoneos #define var

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,8 +6,12 @@ extern char * my_function(int arg1, int arg2);
 int kernel_main(void) {
 
   uart_init();
-
-  uart_puts("Hello World");
+  #ifdef __is_zeonos_kernel
+    uart_puts("Hello World zeonos..!");
+  #else
+    uart_puts("Hello World stranger..!");
+  #endif
+  
   uart_puts(my_function(get_1(), 0));
   return 0;
 }
